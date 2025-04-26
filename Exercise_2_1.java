b6ccd96d650351691e1abf870c99a95b03410deb
Sujung <163294647+Sujung1201@users.noreply.github.com>
2025-04-26 12:49:26 -0500

    Assignment record

---
+++ Exercise_2_1.java
@@ -0,0 +1,7 @@
+public class Exercise_2_1 {
+    public static void main(String[] args) {
+        int day_s = 86400;
+
+        System.out.println(day_s * 365);
+    }
+}
---
+++ Exercise_2_2.java
@@ -0,0 +1,12 @@
+public class Exercise_2_2 {
+    public static void main(String[] args) {
+
+        System.out.println("  | 1  2  3  4  5 ");
+        System.out.println("_________________");
+        System.out.println("1 | 1  2  3  4  5");
+        System.out.println("2 | 2  4  6  8 10");
+        System.out.println("3 | 3  6  9 12 15");
+        System.out.println("4 | 4  8 12 16 20");
+        System.out.println("5 | 5 10 15 20 25");
+    }
+}
